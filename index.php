28e0b98567ea60708102f40d82090d2d780026cc
jonfetahu <84459838+jonfetahu@users.noreply.github.com>
2021-05-19 16:31:14 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Airport</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FREEHTML5.CO" />
+
+  <!-- 
+	//////////////////////////////////////////////////////
+
+	FREE HTML5 TEMPLATE 
+	DESIGNED & DEVELOPED by FREEHTML5.CO
+		
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+	<!-- <link href='https://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700italic,900,700,900italic' rel='stylesheet' type='text/css'> -->
+
+	<!-- Stylesheets -->
+	<!-- Dropdown Menu -->
+	<link rel="stylesheet" href="css/superfish.css">
+	<!-- Owl Slider -->
+	<!-- <link rel="stylesheet" href="css/owl.carousel.css"> -->
+	<!-- <link rel="stylesheet" href="css/owl.theme.default.min.css"> -->
+	<!-- Date Picker -->
+	<link rel="stylesheet" href="css/bootstrap-datepicker.min.css">
+	<!-- CS Select -->
+	<link rel="stylesheet" href="css/cs-select.css">
+	<link rel="stylesheet" href="css/cs-skin-border.css">
+
+	<!-- Themify Icons -->
+	<link rel="stylesheet" href="css/themify-icons.css">
+	<!-- Flat Icon -->
+	<link rel="stylesheet" href="css/flaticon.css">
+	<!-- Icomoon -->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+	
+	<!-- Style -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+<style>
+	body{
+		color: #cccc;
+	}
+</style>
+</head>
+<body>
+
+	<div id="fh5co-wrapper">
+	<div id="fh5co-page">
+	<div id="fh5co-header">
+	<a href="#" style="position: absolute;top: 8px;"> <img src="images/logooo.png" width="300" alt=""> </a>
+		<header id="fh5co-header-section">
+			<div class="container">
+				<div class="nav-header">
+			
+					<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+					<nav id="fh5co-menu-wrap" role="navigation">
+						<ul class="sf-menu" id="fh5co-primary-menu">
+							<li><a href="index.php">Home</a></li>
+							<li><a href="contact.php">Contact</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</header>
+		
+	</div>
+	<!-- end:fh5co-header -->
+	<div class="fh5co-parallax" style="background-image: url(images/airport2.jpg);height: 100%;" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 col-md-offset-0 col-sm-12 col-sm-offset-0 col-xs-12 col-xs-offset-0 text-center fh5co-table">
+					<div class="fh5co-intro fh5co-table-cell">
+						
+						<div id="booking" class="section">
+							<div class="section-center">
+								<div class="container">
+									<div class="row">
+										<div class="booking-form">
+
+										<h5> ARRIVALS</h5></a>
+											<h6> <a href="departures.php" >DEPARTURES</h6></a>
+											
+											<table class="table table-hover table-dark">
+												
+												
+												<thead >
+													<tr>
+													<th scope="col"> </th>
+													<th scope="col">FLIGHT</th>
+													<th scope="col">AIRLINES</th>
+													<th scope="col">FROM</th>
+													<th scope="col">DATE</th>
+													<th scope="col">SCHEDULED</th>
+													<th scope="col">ETA</th>
+													<th scope="col">STATUS</th>
+													</tr>
+												</thead>
+												<tbody>
+													<tr>
+													<th><img src="images/austria.png" width="50" height="30" ></th>
+													<td>OS769</td>
+													<td>AUSTRIAN AIRLINES</td>
+													<td>Vienna</td>
+													<td>18 May</td>
+													<td>00:05</td>
+													<td>23:41</td>
+													<td>ARRIVED 23:49</td>
+													</tr> 
+
+
+													 <tr>
+													<th><img src="images/swiss.png" width="50" height="30" ></th>
+													<td>LX1430</td>
+													<td>SWISS AIRLINES</td>
+													<td>Geneve</td>
+													<td> 19 May	</td>
+													<td>12:10	</td>
+													<td></td>
+													<td>SCHEDULED</td>
+													</tr>  
+
+
+													<tr>
+													<th><img src="images/eurowings.png" width="50" height="30" ></th>
+													<td>EW6617</td>
+													<td>EUROWINGS</td>
+													<td>Stuttgart</td>
+													<td>19 May</td>
+													<td>09:10</td>
+													<td>09:08</td>
+													<td>ARRIVED 09:11</td>
+													</tr> 
+
+
+													<tr>
+													<th><img src="images/wizzair.jpg" width="50" height="30" ></th>
+													<td>W97792</td>
+													<td>WIZZ AIR UK</td>
+													<td>London</td>
+													<td>19 May</td>
+													<td>10:35</td>
+													<td></td>
+													<td>CANCELLED</td>
+													</tr>
+													
+
+													<tr>
+													<th><img src="images/turkish.jpg" width="50" height="30" ></th>
+													<td>TK7876</td>
+													<td>TURKISH AIRLINES</td>
+													<td>Istanbul</td>
+													<td>19 May</td>
+													<td>13:40</td>
+													<td></td>
+													<td>CANCELLED</td>
+													</tr>
+													
+
+													<tr>
+													<th><img src="images/easy.png" width="50" height="30" ></th>
+													<td>EZS1483</td>
+													<td>EASYJET</td>
+													<td>Geneve</td>
+													<td>20 May</td>
+													<td>08:35</td>
+													<td></td>
+													<td>SCHEDULED</td>
+													</tr>
+													
+
+													<tr>
+													<th><img src="images/ede.png" width="50" height="30" ></th>
+													<td>WK402</td>
+													<td>EDELWEISS AIR</td>
+													<td>Zurich</td>
+													<td>20 May</td>
+													<td>08:50</td>
+													<td></td>
+													<td>SCHEDULED</td>
+													</tr>
+													
+
+													<tr>
+													<th><img src="images/chair.png" width="50" height="30" ></th>
+													<td>GM600</td>
+													<td>CHAIR AIRLINES</td>
+													<td>Zurich</td>
+													<td>20 May</td>
+													<td>10:50</td>
+													<td></td>
+													<td>SCHEDULED</td>
+													</tr>
+													
+
+													
+													
+												</tbody>
+												</table>
+																							
+											
+										</div>
+										
+									<!-- </div>
+									 <div id="cont_02dafb2fcbc02629a0f8fadd09547d15"><script type="text/javascript" 
+										async src="https://www.theweather.com/wid_loader/02dafb2fcbc02629a0f8fadd09547d15"></script></div> -->
+								</div> 
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+
+	
+	
+
+	
+	
+
+	 <!-- <footer id="footer" class="fh5co-bg-color">
+		 <div class="container">
+			<div class="row">
+				<div class="col-md-3">
+					<div class="copyright">
+						<p><small>&copy; 2016 Free HTML5 Template. <br> All Rights Reserved. <br>
+						Designed by <a href="http://freehtml5.co" target="_blank">FreeHTML5.co</a> <br> Demo Images: <a href="http://unsplash.com/" target="_blank">Unsplash</a></small></p>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="row">
+						<div class="col-md-3">
+							<h3>Company</h3>
+							<ul class="link">
+								<li><a href="#">About Us</a></li>
+								<li><a href="#">Hotels</a></li>
+								<li><a href="#">Customer Care</a></li>
+								<li><a href="#">Contact Us</a></li>
+							</ul>
+						</div>
+						<div class="col-md-3">
+							<h3>Our Facilities</h3>
+							<ul class="link">
+								<li><a href="#">Resturant</a></li>
+								<li><a href="#">Bars</a></li>
+								<li><a href="#">Pick-up</a></li>
+								<li><a href="#">Swimming Pool</a></li>
+								<li><a href="#">Spa</a></li>
+								<li><a href="#">Gym</a></li>
+							</ul>
+						</div>
+						<div class="col-md-6">
+							<h3>Subscribe</h3>
+							<p>Sed cursus ut nibh in semper. Mauris varius et magna in fermentum. </p>
+							<form action="#" id="form-subscribe">
+								<div class="form-field">
+									<input type="email" placeholder="Email Address" id="email">
+									<input type="submit" id="submit" value="Send">
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<ul class="social-icons">
+						<li>
+							<a href="#"><i class="icon-twitter-with-circle"></i></a>
+							<a href="#"><i class="icon-facebook-with-circle"></i></a>
+							<a href="#"><i class="icon-instagram-with-circle"></i></a>
+							<a href="#"><i class="icon-linkedin-with-circle"></i></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div> 
+		
+	</footer>  -->
+
+	</div>
+	<!-- END fh5co-page -->
+
+	</div>
+	<!-- END fh5co-wrapper -->
+	
+	<!-- Javascripts -->
+	<script src="js/jquery-2.1.4.min.js"></script>
+	<!-- Dropdown Menu -->
+	<script src="js/hoverIntent.js"></script>
+	<script src="js/superfish.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Counters -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Owl Slider -->
+	<!-- // <script src="js/owl.carousel.min.js"></script> -->
+	<!-- Date Picker -->
+	<script src="js/bootstrap-datepicker.min.js"></script>
+	<!-- CS Select -->
+	<script src="js/classie.js"></script>
+	<script src="js/selectFx.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+
+	<script src="js/custom.js"></script>
+
+</body>
+</html>
